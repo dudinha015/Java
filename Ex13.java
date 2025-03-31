a1f5742a8665186afc452da1882bb5377f60bc50
dudinha015 <maria.arruda1a@gmail.com>
2025-03-31 08:50:32 -0300

    Exercicio 13 - Terrenos

---
+++ Ex13.java
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+12. Faça um programa que leia a largura e o comprimento de um terreno
+retangular, calculando e mostrando a sua área em m². O programa também
+deve mostrar a classificação desse terreno, de acordo com a lista abaixo:
+
+- Abaixo de 100m² = TERRENO POPULAR
+- Entre 100m² e 500m² = TERRENO MASTER
+- Acima de 500m² = TERRENO VIP
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex13
+{
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		//Declarando variáveis 
+		int comprimento, largura;
+		String terrenoPopular, terrenoMaster, terrenoVip;
+		
+		//Entrada de dados
+		System.out.println("Entre com o comprimento: ");
+		comprimento = ler.nextInt();
+		
+		System.out.println("Entre com a largura: ");
+		largura = ler.nextInt();
+		
+	
+		//Saída de dados
+		
+		if (comprimento <= 100) {
+		System.out.printString("terrenoPopular");
+	    } if (comprimento <= 500) {
+		System.out.printString ("terrenoMaster");
+		
+	     if (comprimento >= 500)
+	System.out.printString ("terrenoVip");
+		}
+	}
+}
