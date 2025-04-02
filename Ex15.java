7fda32e8d9bdefed689f17b74fbcca03e7949ffb
dudinha015 <maria.arruda1a@gmail.com>
2025-04-02 09:59:52 -0300

    Ex15.java

--- Ex15.java
+++ Ex15.java
@@ -1,25 +1,21 @@
 /******************************************************************************
-15) Escreva um programa que calcule a soma dos números de 1 a 15.
-
+15. Escreva um programa que calcule a soma dos números de 1 a 15.
 *******************************************************************************/
 import java.util.Scanner;
 public class Ex15
 
 {
-	public static void main(String[] args) {
-		Scanner ler = new Scanner (System.in);
-		int soma = 0, num;
-		
-	System.out.println ("Entre com um número desejado : ");
-	num = ler.nextInt();
-	
-    for (int i = 1; i <=15; i ++)
-    soma = soma + i;
-	 System.out.println ("A soma é: " + soma); 
-	}
+      public static void main(String[] args) {
+      Scanner ler = new Scanner (System.in);
+      int n,resultado=0;
+     
+     
+      for (int i=1 ; i<= 15; i++) {
+      resultado= resultado+i;
+    }
+      System.out.println("O resultado da soma será: " + resultado );
 }
-
-    
+}
     
 
 
