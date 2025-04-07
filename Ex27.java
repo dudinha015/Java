66b093c5d14191cd63e7b189d5d4620392164461
dudinha015 <maria.arruda1a@gmail.com>
2025-04-07 11:17:42 -0300

    Exercicio 27 - Criar um prgrama que mostre o nome de cinco pessoas

---
+++ Ex27.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+27. Faça um programa, utilizando while, que permita o usuário escrever o nome de cinco pessoas e os mostre na tela.
+*******************************************************************************/
+
+import java.util.Scanner;
+
+public class Ex27 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int nome = 1;
+        String nomeDoUsuario;
+
+        while (nome <= 5) {
+            System.out.println("Escreva o " + nome + "º nome do usuário: ");
+            nomeDoUsuario = ler.nextLine(); 
+            nome++; 
+        }
+    }
+}    
