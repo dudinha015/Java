58043884f08f83c8907ad4d34e222a06322d0439
dudinha015 <maria.arruda1a@gmail.com>
2025-04-07 15:06:31 -0300

    Exercicio 34 - exemplos de estrutura de repetição DOWHILE

---
+++ Ex34.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+34. Exemplo de estrutura de repetição DOWHILE
+*******************************************************************************/
+import java.util.Scanner;
+ public class Ex34
+ {
+      public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int contador = 0;
+        
+        do {
+            System.out.println(contador);
+            contador++;
+        }
+       
+       
+        while (contador <= 5); {
+            
+         
+        }
+     }    
+}
